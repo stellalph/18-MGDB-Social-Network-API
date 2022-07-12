a28eeecaa4e8d5fda53ae78389aa626986ee7163
Stella Ling <stellalph2@gmail.com>
2022-07-13 00:21:43 +0930

    add code to server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+// Require express and mongoose
+const express = require ('express');
+const mongoose = require ('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// tells mongoose which database to connect to 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
