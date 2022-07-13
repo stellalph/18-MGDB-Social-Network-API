cab54a6d549926791ae727dd182a1d8297b6d600
Stella Ling <stellalph2@gmail.com>
2022-07-13 21:29:41 +0930

    set up codes for Thought model

---
+++ models/Thought.js
@@ -0,0 +1,66 @@
+//  Importing dependencies
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+// Reaction is a subdocument of Thought
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        // default value is set to a new ObjectId
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (CreatedAtVal) => moment(createdAtVal).format('MM DD, YYYY [at] hh:mm a')
+    }
+},
+
+    {
+        toJSON: {
+            getters: true
+        }
+    });
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;  
