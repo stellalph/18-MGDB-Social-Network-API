c9958cc05ffb27f5896cc5a6e99844ba843c4819
Stella Ling <stellalph2@gmail.com>
2022-07-14 22:47:06 +0930

    amend package.json and server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(require('./routes'));
 
 // tells mongoose which database to connect to 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/18-MGDB-Social-Network-Api', {
-  // useFindAndModify: false,
+
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
